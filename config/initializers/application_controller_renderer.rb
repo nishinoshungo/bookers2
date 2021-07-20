3cbaa5b20ed7befdf9d464342773b376fb796558
nishinoshungo <nishishun50@gmail.com>
2021-07-20 03:24:54 +0000

    complete

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '80ae0af8766337b6bf4ba51a090707524f853c5e6317ae8c4e2ca54b8020c09a55a85dbbdcd2ef171c5ea7a5df33ec7eb3c8b0bc8bddcad57d39ea69996b48a5'
